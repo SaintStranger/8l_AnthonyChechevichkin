f1330d2f0cf4311576765677803a0ffc464be778
Antony Chechevichkin <Anton-bopoh@yandex.ru>
2020-09-24 15:07:27 +0300

    Initial Commit

---
+++ 8l_AnthonyChechevichkin/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  8l_AnthonyChechevichkin
+//
+//  Created by Антон Чечевичкин on 24.09.2020.
+//  Copyright © 2020 Антон Чечевичкин. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
